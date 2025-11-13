6d78063f241a9d6d98120e8854a6816e98b781ea
Caro Páez <caropaez64@users.noreply.github.com>
2025-11-13 12:23:51 -0600

    Cambios actualizados en la rama main.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # proyecto_s7
-Proyecto del Sprint 7
+Proyecto del Sprint 7 - Análisis de ventas de vehículos
+
+## Descripción del proyecto
+Esta aplicación web permite a los usuarios explorar interactivamente las ventas de diferentes vechículos así como los datos de los anuncios.
+
+## Funcionalidad
+* Visualización de histogramas para analizar la distribución de precios, modelos, etc.
+* Filtros interactivos mediante casillas.
+* Análisis exploratorio de datos en tiempo real.
+
+## Paquetes utilizads
+Python
+Pandas
+Ploty express
+Streamlit
---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('/Users/CaroPB/Documents/Análisis de datos/Tripleten/Sprint 7/proyecto/proyecto_s7/vehicles_us.csv')
+
+st.header('Construcción de un histograma')
+build_histogram = st.checkbox('Construir histograma')
+if build_histogram:
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+build_dispersion = st.checkbox('Construir una gráfica de dispersión')
+if build_dispersion:
+    st.write('Construir una gráfica de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
